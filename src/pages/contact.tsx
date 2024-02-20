4dc2b38bcf2d5f05ccbb88d25c7edb5898bb4d99
unknown <nnaemekafrank400@gmail.com>
2024-02-21 00:19:28 +0100

    changed email

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -150,7 +150,7 @@ export default function About({ location }) {
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope-fill w-6 h-6" viewBox="0 0 16 16"> <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/> </svg>
                             </div>
 
-                            <div><a href="mailto:info@sportycredit.com">info@sportycredit.com</a></div>
+                            <div><a href="mailto:support@sportycredit.com">support@sportycredit.com</a></div>
                         </div>
 
 
